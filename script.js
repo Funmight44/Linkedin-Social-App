6870918945246db359e5a7359a9e85eaa5badbf5
Funmight44 <akanofunmilayo44@gmail.com>
2023-12-29 13:43:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var profileMenu = document.getElementById("profileMenu");
+
+function toggleMenu(){
+    profileMenu.classList.toggle("open-menu");
+}
+
+// sidebar activity
+
+var sideActivity = document.getElementById("sidebarActivity");
+var moreLink = document.getElementById("ShowMoreLink");
+
+function toggleActivity(){
+    sideActivity.classList.toggle("open-activity");
+}
